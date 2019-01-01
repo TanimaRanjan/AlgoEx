99a8c3a05f2c41be31761d934641de0b5c5f6c51
Tanima Ranjan <tanimaranjan@gmail.com>
2019-01-01 14:41:52 -0500

    anagram

--- chunk.js
+++ chunk.js
@@ -6,3 +6,17 @@
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+  
+    while (index < array.length) {
+      chunked.push(array.slice(index, index + size));
+      index += size;
+    }
+  
+    return chunked;
+  }
+
