aa2c4247683907e9b2e23dd2650a5c9344724fc0
Tanima Ranjan <tanimaranjan@gmail.com>
2019-01-03 20:26:05 -0500

    Capitalize

--- capitalize.js
+++ capitalize.js
@@ -6,3 +6,20 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+    let result = str[0].toUpperCase()
+
+    for(let i=1; i< str.length; i++) {
+        if(str[i-1] === ' ') {
+            result += str[i].toUpperCase()
+        } else {
+            result += str[i]
+        }
+    }
+    console.log(result)
+}
+
+capitalize('hi there, how is it going ? ')
+
+capitalize('my name is tanima ranjan ')
