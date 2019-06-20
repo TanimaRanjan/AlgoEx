c3494fb946ddfb8c546bae8f119b42e07ba1753e
Tanima Ranjan <tanimaranjan@gmail.com>
2019-06-20 14:30:49 -0400

    Sum of Two Arrays to a values

---
+++ sumOfTwo.js
@@ -0,0 +1,22 @@
+/* 
+You have two integer arrays, a and b, and an integer target value v.
+Determine whether there is a pair of numbers, where one number is taken from a and the other from b, 
+that can be added together to get a sum of v. Return true if such a pair exists, otherwise return false.
+
+Example For a = [1, 2, 3], b = [10, 20, 30, 40], and v = 42, the output should be sumOfTwo(a, b, v) = true.
+*/
+
+const sumOfTwo = (a, b, v) => {
+
+    for(let i =0; i< a.length; i++) {
+        for(let j=0;j<b.length; j++) {
+
+            if(a[i] + b[j] === v) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42))
