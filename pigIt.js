676580effa28cb67dd42ed7bf222d44d37c37fd1
Tanima Ranjan <tanimaranjan@gmail.com>
2019-06-20 14:33:36 -0400

    Correction - Adding vowel - way

--- pigIt.js
+++ pigIt.js
@@ -1,4 +1,7 @@
-// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+/*
+ Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
+Leave punctuation marks untouched.
+*/
 
 
 function pigIt(string) {
@@ -6,11 +9,16 @@ function pigIt(string) {
     var stringArr = string.split(' ');
     var strTemp = '';
     var finalString = '';
+    var vowelReg = /[aeiou]/
     for (var i = 0; i < stringArr.length; i++) {
         strTemp = '';
-        //strTemp = stringArr[i].substr(1, stringArr[i].length-1) + stringArr[i][0] + 'ay';
-        strTemp = stringArr[i].slice(1).concat(stringArr[i][0].concat('ay'));
-        finalString = finalString + strTemp + ' ';
+        if(vowelReg.test(stringArr[i][0])) {
+            strTemp = `${stringArr[i]}way`
+        } else { 
+            strTemp = `${stringArr[i].slice(1)}${stringArr[i][0]}ay`
+        }
+        finalString += strTemp + ' ';
+
     }
     return finalString;
 
