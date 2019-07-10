0324d27fb9c273347eca829482a6a0af1cc56db7
Tanima Ranjan <tanimaranjna@gmail.com>
2019-07-09 20:37:09 -0400

    Binary Search Tree

---
+++ binarySearchTree.js
@@ -0,0 +1,19 @@
+/* 
+    Binary Search Tree 
+*/
+
+class Node {
+    constructor(item) {
+        this.item = item
+        this.left = left
+        this.right = right
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+
+    insert(item)
+}
