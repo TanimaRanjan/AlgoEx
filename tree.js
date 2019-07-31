1f6cc21a4457c2c1d4526624185afdc8eb9a250b
TanimaRanjan <tanimaranjan@gmail.com>
2019-07-31 09:17:27 -0400

    Tree creation

--- tree.js
+++ tree.js
@@ -9,3 +9,71 @@
 // 3) Implement 'traverseBF' and 'traverseDF'
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
+
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.children = [];
+    }
+
+    add(data) {
+        this.children.push(new Node(data));
+    }
+
+    remove(data) {
+        this.children = this.children.filter((child) => child.data !== data )
+    }
+}
+
+
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    traverseBF() {
+        let arr = [this.root];
+        let traverse = []
+
+        while(arr.length) {
+            let node = arr.shift();
+            traverse.push(node.data);
+            arr.push(...node.children)
+        }
+        return traverse;
+    }
+
+    traverseDF() {
+        let arr = [this.root];
+        let traverse = [];
+
+        while(arr.length) {
+            let node = arr.shift();
+            traverse.push(node.data);
+            arr.unshift(...node.children);
+        } 
+        return traverse;
+    }
+}
+
+const node = new Node(20);
+node.add(0);
+node.add(40);
+node.add(15);
+
+const tree = new Tree();
+tree.root = node;
+
+node1 = node.children[0];
+node1.add(12);
+node1.add(-2);
+node1.add(1);
+
+node2 = node.children[2];
+node2.add(-2);
+
+
+console.log(tree.traverseBF());
+
+console.log(tree.traverseDF());
