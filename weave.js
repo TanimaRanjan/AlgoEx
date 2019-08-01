34e4b9f54ac9ce9fed92fecb225e40777e3eb441
TanimaRanjan <tanimaranjan@gmail.com>
2019-08-01 15:47:11 -0400

    Weave 2 queue together

--- weave.js
+++ weave.js
@@ -15,9 +15,92 @@
 //    queueOne.add(2);
 //    const queueTwo = new Queue();
 //    queueTwo.add('Hi');
-//    queueTwo.add('There');
-//    const q = weave(queueOne, queueTwo);
-//    q.remove() // 1
-//    q.remove() // 'Hi'
-//    q.remove() // 2
-//    q.remove() // 'There'
+//    queueTwo.add('There')
+
+class Queue {
+
+    constructor() {
+        this.item = []
+    }
+
+    enQueue(item) {
+        
+        this.item.push(item)
+    }
+
+    deQueue() {
+        var olditem = this.item.splice(0, 1)
+        return olditem[0]
+    }
+
+    isEmpty() {
+        return this.item.length === 0 
+    }
+
+    printQueue() {
+        var str = "" 
+
+        for(var i=0; i<this.item.length; i++ ) {
+            str += this.item[i] + " "
+        }
+        return str
+    }
+
+ }
+
+
+const queueOne = new Queue();
+queueOne.enQueue(1);
+queueOne.enQueue(2);
+queueOne.enQueue(3);
+queueOne.enQueue(4);
+
+
+const queueTwo = new Queue();
+queueTwo.enQueue('Hi');
+queueTwo.enQueue('There');
+queueTwo.enQueue('you');
+queueTwo.enQueue('Hi');
+queueTwo.enQueue('There');
+queueTwo.enQueue('you');
+queueTwo.enQueue('Hi');
+queueTwo.enQueue('There');
+queueTwo.enQueue('you');
+queueTwo.enQueue('Hi');
+queueTwo.enQueue('There');
+queueTwo.enQueue('you');
+queueTwo.enQueue('Hi');
+queueTwo.enQueue('There');
+queueTwo.enQueue('you');
+
+const weave = (q1, q2) => {
+
+    let qFinal = new Queue();
+
+    while(!q1.isEmpty() || !q2.isEmpty()) {
+        if(!q1.isEmpty()) {
+            qFinal.enQueue(q1.deQueue());
+        } 
+        if(!q2.isEmpty()) {
+            qFinal.enQueue(q2.deQueue());
+        }
+        console.log(qFinal.printQueue());
+    }
+
+    return qFinal;
+
+
+
+}
+
+const q = weave(queueOne, queueTwo);
+console.log(q.deQueue()); 
+console.log(q.deQueue()); 
+console.log(q.deQueue()); 
+console.log(q.deQueue()); 
+console.log(q.deQueue()); 
+console.log(q.deQueue()); 
+console.log(q.deQueue()); 
+console.log(q.deQueue()); 
+
+
