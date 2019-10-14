f8a39d1b9a9cb5a638a43a8c1640ef79856838b7
TanimaRanjan <tanimaranjan@gmail.com>
2019-10-14 12:30:08 -0400

    Is Palindrome

--- isPalindrome.js
+++ isPalindrome.js
@@ -13,3 +13,22 @@ console.log(isPalindrome('AaBaa'))
 console.log(isPalindrome('Abac'))
 
 console.log(isPalindrome('Aa .,8757Baa'))
+
+
+function isPalindrome2(n) {
+	let temp = n
+	let rev = 0
+	while(temp>0) {
+		rev = rev*10 + temp%10
+		temp = Math.floor(temp/10)
+	}
+	return rev === n
+}
+
+
+console.log(isPalindrome2(838))
+console.log(isPalindrome2(77))
+console.log(isPalindrome2(95159))
+console.log(isPalindrome2(839))
+console.log(isPalindrome2(4234))
+console.log(isPalindrome2(13))
