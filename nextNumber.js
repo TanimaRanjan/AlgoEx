1b6f829b65952cce249f5b0f1b13471473f3696f
TanimaRanjan <tanimaranjan@gmail.com>
2019-10-24 22:49:46 -0400

    Next Number

---
+++ nextNumber.js
@@ -0,0 +1,21 @@
+const nextNumber = (num) => {
+
+    //let str = parseInt(num.toString().split('').sort().reverse().join(''))
+    let found = false
+    while(!found) {
+        
+        let temp = parseInt(num.toString().split(''))
+        console.log(temp)
+        
+
+        console.log(str)
+    }
+}
+
+nextNumber(19)
+
+nextNumber(3542)
+
+nextNumber(5432)
+
+nextNumber(58943)
