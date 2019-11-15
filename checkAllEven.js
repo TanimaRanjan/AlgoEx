d04e29d6396651426e3c15aaa6b0e64914d686b9
TanimaRanjan <tanimaranjan@gmail.com>
2019-11-15 17:34:19 -0500

    Check if all in array are even

---
+++ checkAllEven.js
@@ -0,0 +1,15 @@
+/*Create a function that takes in an array and returns true if all its values are even, and false otherwise.
+
+Not a big deal, your friend says. He writes the following code:*/
+
+function checkAllEven(arr) {
+  return arr.every(x => x % 2 === 0)
+
+}
+// The code above leads to a Reference Error, with x being undefined. Fix the code above so that all tests pass:
+
+//Examples
+console.log(checkAllEven([1, 2, 3, 4])) //➞ false
+console.log(checkAllEven([2, 4, 6])) //➞ true
+console.log(checkAllEven([5, 6, 8, 10])) //➞ false
+console.log(checkAllEven([-2, 2, -2, 2])) //➞ true
