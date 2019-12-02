bc862175952f0621ba88440b470e26e4c48fba0f
TanimaRanjan <tanimaranjan@gmail.com>
2019-12-01 21:51:22 -0500

    binary

---
+++ binary.js
@@ -0,0 +1,19 @@
+const binary = (decimal) => {
+
+    let binary = ''
+    let binary2 = ''
+    while(decimal>0) {
+        console.log(decimal%2)
+        binary = binary*10 + decimal%2
+        binary2 = decimal%2 + binary2
+        console.log(binary, binary2)
+        decimal = Math.floor(decimal/2)
+        console.log(decimal)
+    }
+    
+
+}
+binary(1)
+binary(5)
+binary(10)
+binary(100)
