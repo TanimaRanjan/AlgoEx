eab6f2bff26835462fc6819acdeaf0280f3b0b8d
TanimaRanjan <tanimaranjan@gmail.com>
2020-01-28 16:36:16 -0500

    Remove Duplicates

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,19 +1,50 @@
 
-/* Remove duplicates from input array */ 
+// /* Remove duplicates from input array */ 
 
-function removeDuplicates(array) {
+// function removeDuplicates(array) {
 
-    console.log("Remove duplicates from array - ")
-    var arr1 = [];
-    var arrList = {};
-    array.forEach(num => {
-        if (!arrList[num]) {
-            arr1.push(num);
-            arrList[num] = true;
+//     console.log("Remove duplicates from array - ")
+//     var arr1 = [];
+//     var arrList = {};
+//     array.forEach(num => {
+//         if (!arrList[num]) {
+//             arr1.push(num);
+//             arrList[num] = true;
+//         }
+//     });
+//     console.log(arr1);
+// }
+
+
+
+
+var removeDuplicates = function(nums) {
+    
+    let result = []
+    let hash = {}
+    
+    nums.forEach(num => {
+        if(!hash[num]) {
+            result.push(num)
+            hash[num] = true
+         //   console.log(hash, result)
         }
     });
-    console.log(arr1);
+    return result
+};
+
+
+console.log(removeDuplicates([1,3,3,3,1,5,6,7,8,1]));
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    
+    nums.splice(0, nums.length, ...(new Set(nums)))
+
 }
 
-removeDuplicates([1,3,3,3,1,5,6,7,8,1]);
 
