d46bafc0dd059c0673e27d0a5872ff8ac117728b
TanimaRanjan <tanimaranjan@gmail.com>
2020-01-28 16:37:30 -0500

    Convert to binary

--- binary.js
+++ binary.js
@@ -3,14 +3,15 @@ const binary = (decimal) => {
     let binary = ''
     let binary2 = ''
     while(decimal>0) {
-        console.log(decimal%2)
+       // console.log(decimal%2)
         binary = binary*10 + decimal%2
         binary2 = decimal%2 + binary2
-        console.log(binary, binary2)
+       // console.log(binary, binary2)
         decimal = Math.floor(decimal/2)
-        console.log(decimal)
+       // console.log(decimal)
     }
     
+    console.log(binary, binary2)
 
 }
 binary(1)
