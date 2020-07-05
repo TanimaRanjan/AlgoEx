e2213c3d6b602be2d03c0753cec994cd70b8e755
TanimaRanjan <tanimaranjan@gmail.com>
2020-07-05 18:41:34 -0400

    Switch light problme

---
+++ switchLights.js
@@ -0,0 +1,11 @@
+/* N candles are placed in a row, some of them are initially lit. For each candle from the 1st to the Nth the following algorithm is applied:
+ if the observed candle is lit then states of this candle and all candles before it are changed to the opposite. 
+ Which candles will remain lit after applying the algorithm to all candles in the order they are placed in the line?
+
+Example
+
+For a = [1, 1, 1, 1, 1], the output should be switchLights(a) = [0, 1, 0, 1, 0].*/
+
+const switchLights = () => {
+    
+}
