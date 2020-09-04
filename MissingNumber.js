35012b7d029631099814d091b407cf14ab9705d4
TanimaRanjan <tanimaranjan@gmail.com>
2020-09-04 18:43:12 -0400

    Find missing no

---
+++ MissingNumber.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let total = Math.ceil( nums.length * ((nums.length+1)/2))    
+    let sum = 0
+    for(let i=0;i<nums.length;i++) {
+        sum += nums[i]
+    }
+
+    return total - sum 
+};
